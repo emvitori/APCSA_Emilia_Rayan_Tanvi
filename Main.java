7d5306ed7f4f4559107233677e7c49613fb41d88
jmantel-edu <jmantel@my.waketech.edu>
2026-09-22 17:05:56 +0000

    Clean up and disallow entering negative years

--- Main.java
+++ Main.java
@@ -2,33 +2,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
 // creates a new scanner object to take in user input
         int i = 0;
         CollegeConnector user = new CollegeConnector();
+
 // creates a new CollegeConnector object to store user input
         System.out.println();
-        System.out.println("Are you going to college?: ");
+        System.out.println("Are you going to college? (Yes/No): ");
         user.setCollege(input.nextLine());
         System.out.println();
+
 // checks if the user is going to college, if not, asks if they are sure and if they want their kids to look at the menu prices
         if (user.getCollege().equals("no") || user.getCollege().equals("No")) {
-            System.out.println("are you sure?");
+            System.out.println("are you sure? (Yes/No)");
             user.setCertainty(input.nextLine());
             System.out.println();
+
 // checks if the user is sure they are not going to college, if not, asks if they want their kids to look at the menu prices
             if (user.getCertainty().equals("yes") || user.getCertainty().equals("Yes")) {
-                System.out.println("Do you want your kids to look at the menu prices??");
+                System.out.println("Do you want your kids to look at the menu prices? (Yes/No)");
                 user.setMenu(input.nextLine());
                 System.out.println();
                 if (user.getMenu().equals("yes") || user.getMenu().equals("Yes")) {
                     System.out.println("ok, bye");
                     System.exit(0);
+
 // if the user is sure they are not going to college and they don't want their kids to look at the menu prices, the program will exit
                 } else {
                     i += 1;
                     System.out.println("ok, let's continue");
                     while (i < 3) {
-                        System.out.println("Are you going to college?: ");
+                        System.out.println("Are you going to college? (Yes/No): ");
                         user.setCollege(input.nextLine());
                         System.out.println();
                         i += 1;
@@ -39,14 +44,20 @@ public class Main {
                     }
                 }
             }
+
 // if the user is not sure they are not going to college, the program will continue
         } else {
             System.out.println("ok, let's continue");
         }
+
 // asks the user how long they are planning to go to college and stores the input in the CollegeConnector object
-        System.out.println("How long are you planning to go to College in years?:");
+        System.out.println("How many years are you planning to go to college? ");
         user.setLength(input.nextInt());
+        while (user.getLength() <= 0) {
+            System.out.println("Not a valid input, please try again: ");
+        }
         System.out.println();
+
 // checks if the user is planning to go to college for more than 3 years, if so, asks if they are interested in Med or Law school and stores the input in the CollegeConnector object
         if (user.getLength() > 3) {
             System.out.println("Specify if you are interested in Med or Law school?: ");
@@ -65,6 +76,7 @@ public class Main {
         else {
             user.setGraduate("College");
         }
+
 // asks the user how much they are willing to spend on college and stores the input in the CollegeConnector object 
         System.out.println("How much would you be willing to spend on college?: ");
         user.setCost(input.nextInt());
@@ -95,6 +107,7 @@ public class Main {
         if (user.getModel().equals("Military") || user.getModel().equals("military")) {
             user.setModel("Military");
         }
+
 // asks the user how many students they would like to be around and stores the input in the CollegeConnector object
         System.out.println("How many students would you like to be around?:");
         user.setEnvironment(input.nextInt());
@@ -108,6 +121,7 @@ public class Main {
         else if (user.getEnvironment() < 15000) {
             user.setEnvironmentSize("Urban");
         }
+
 // asks the user if they would like to study abroad and stores the input in the CollegeConnector object
         System.out.println("Would you like to study Abroad?:");
         user.setLocation(input.next());
